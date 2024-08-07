75c7d444b55590a44051cdd742a197a56572e3d1
sserh <serhios@bk.ru>
2024-08-07 18:15:44 +0700

    - The first test method added

--- src/test/java/ru/raccoon/netologydiploma/NetologyDiplomaApplicationTests.java
+++ src/test/java/ru/raccoon/netologydiploma/NetologyDiplomaApplicationTests.java
@@ -1,13 +1,47 @@
 package ru.raccoon.netologydiploma;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.testcontainers.containers.GenericContainer;
+import ru.raccoon.netologydiploma.dbentities.FileInfo;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NetologyDiplomaApplicationTests {
 
+    private static final GenericContainer<?> diplomaApp = new GenericContainer<>("diploma:1.5")
+            .withExposedPorts(8090);
+
+    String hostname = "localhost";
+    String requestString = "/cloud/list?limit=3";
+    String loginString = "/login";
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @BeforeAll
+    public static void setup() {
+        diplomaApp.start();
+    }
+
     @Test
-    void contextLoads() {
+    void checkAccessDenied() {
+        ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://" + hostname + ":" + diplomaApp.getMappedPort(8090) + requestString, Void.class);
+        Assertions.assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode());
+    }
+
+    @Test
+    void CheckLogin() {
+        ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://" + hostname + ":" + diplomaApp.getMappedPort(8090) + loginString, Void.class);
     }
 
 }
